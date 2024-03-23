4bf005982559a4fc5985e3caf372f231eaf929d5
AmeliaLis <lis.amelia.maria@gmail.com>
2024-03-23 19:44:06 +0100

    update powershell script  and playbook

--- ansible-db/start_vm.ps1
+++ ansible-db/start_vm.ps1
@@ -1,4 +1,7 @@
 VBoxManage startvm db-machine-1 --type headless
+Start-Sleep -Seconds 10
 VBoxManage startvm db-machine-2 --type headless
+Start-Sleep -Seconds 10
 VBoxManage startvm db-machine-3 --type headless
+Start-Sleep -Seconds 10
 VBoxManage startvm db-machine-4 --type headless

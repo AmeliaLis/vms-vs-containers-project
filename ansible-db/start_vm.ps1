0e331dc09a355ae0e255c8dc7d90e23c0329626a
AmeliaLis <lis.amelia.maria@gmail.com>
2024-03-23 18:54:11 +0100

    refactor to use powershell script

---
+++ ansible-db/start_vm.ps1
@@ -0,0 +1,4 @@
+VBoxManage startvm db-machine-1 --type headless
+VBoxManage startvm db-machine-2 --type headless
+VBoxManage startvm db-machine-3 --type headless
+VBoxManage startvm db-machine-4 --type headless

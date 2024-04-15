d5825e12612f1b7845a3647adfa5451a574e9110
AmeliaLis <lis.amelia.maria@gmail.com>
2024-04-15 18:39:41 +0200

    test fix etc mysql conf

--- docker-db/docker-1/custom_entrypoint.sh
+++ docker-db/docker-1/custom_entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Add custom configuration to my.cnf
-echo "[mysqld]" >> /etc/mysql/my.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
+echo "bind-address = 0.0.0.0" >> /etc/my.cnf
 
 # Execute the CMD from Dockerfile
 exec "$@"
